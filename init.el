1c876a5a7aa53413764e0747a420fa3d6fa980ac
rs0604 <shima19850604mail@gmail.com>
2020-11-17 18:39:20 +0900

    テーマの変更

--- init.el
+++ init.el
@@ -103,7 +103,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-manegarm t)
+  (load-theme 'doom-sourcerer t)
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config)
   (doom-themes-org-config))
