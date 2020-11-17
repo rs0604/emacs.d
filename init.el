d11d6caaea16f1eac0d755209bc835dc7eb37b71
rs0604 <shima19850604mail@gmail.com>
2020-11-17 21:41:52 +0900

    web-modeの不要な色設定を削除

--- init.el
+++ init.el
@@ -370,35 +370,6 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]$"	. web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb$"	. web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?$"	. web-mode))
-
-  ;; 色の設定
-  ;;(custom-set-faces
-  ;; '(web-mode-doctype-face
-  ;;   ((t (:foreground "#82AE46"))))
-  ;; '(web-mode-html-tag-face
-  ;;   ((t (:foreground "#E6B422" :weight bold))))
-  ;; '(web-mode-html-attr-name-face
-  ;;   ((t (:foreground "#C97586"))))
-  ;; '(web-mode-html-attr-value-face
-  ;;   ((t (:foreground "#82AE46"))))
-  ;; '(web-mode-comment-face
-  ;;   ((t (:foreground "#D9333F"))))
-  ;; '(web-mode-server-comment-face
-  ;;   ((t (:foreground "#D9333F"))))
-  ;; '(web-mode-css-rule-face
-  ;;   ((t (:foreground "#A0D8EF"))))
-  ;; '(web-mode-css-pseudo-class-face
-  ;;   ((t (:foreground "#FF7F00"))))
-  ;; '(web-mode-css-at-rule-face
-  ;;   ((t (:foreground "#FF7F00"))))
-  ;; )
-  ;; インデント数
-  ;; (setq web-mode-html-offset	2)
-  ;; (setq web-mode-css-offset	2)
-  ;; (setq web-mode-script-offset	2)
-  ;; (setq web-mode-php-offset	2)
-  ;; (setq web-mode-java-offset	2)
-  ;; (setq web-mode-asp-offset	2)
   )
 
 ;; ---------------------------------------- magit
