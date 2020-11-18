df8a5aaf8c61ae3cdb6d357b276375a0446c91d0
SHIMA Ryosuke <gwienn0604@gmail.com>
2020-11-18 22:49:28 +0900

    テーマをもう少し明るいのに変更

--- init.el
+++ init.el
@@ -111,7 +111,7 @@
   :config
   (setq doom-themes-enable-bold t
         doom-themes-enable-italic t)
-  (load-theme 'doom-sourcerer t)
+  (load-theme 'doom-gruvbox t)
   (doom-themes-visual-bell-config)
   (doom-themes-neotree-config)
   (doom-themes-org-config))
